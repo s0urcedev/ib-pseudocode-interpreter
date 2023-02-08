210f31be1bd85ad3b79fa801b2b73f4f80e765c5
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-02-08 19:54:02 +0000

    Update main.py

--- static/interpreter/main.py
+++ static/interpreter/main.py
@@ -1,14 +1,16 @@
 from code_entities import Code
 
-if __name__ == "__main__":
-    code = ""
-    while True:
-        try:
-            line = input()
-            if chr(3) not in line:
-                code += line + "\n"
-            else:
-                break
-        except:
-            break
-    Code(code).run()
+print(1)
+
+# if __name__ == "__main__":
+#     code = ""
+#     while True:
+#         try:
+#             line = input()
+#             if chr(3) not in line:
+#                 code += line + "\n"
+#             else:
+#                 break
+#         except:
+#             break
+#     Code(code).run()
