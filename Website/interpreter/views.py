40eef050c40e7bbc031611376e06a3b8d3f192a5
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-02-08 18:31:05 +0000

    Update views.py

--- Website/interpreter/views.py
+++ Website/interpreter/views.py
@@ -5,18 +5,18 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def index(request):
-    # if request.method == "GET":
-    return render(request, "index.html")
-    # elif request.method == "POST":
-    #     data: dict[str, str] = json.loads(request.body.decode())
-    #     sb: subprocess.CompletedProcess = subprocess.run(
-    #         ['python', '../Interpreter/main.py'],
-    #         input=f"{data['code']}\n{chr(3)}\n{data['input']}".encode(),
-    #         capture_output=True
-    #     )
-    #     return HttpResponse(json.dumps({
-    #         "stdout": sb.stdout.decode(),
-    #         "stderr": sb.stderr.decode()
-    #     }))
+def index(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        return render(request, "index.html")
+    elif request.method == "POST":
+        data: dict[str, str] = json.loads(request.body.decode())
+        sb: subprocess.CompletedProcess = subprocess.run(
+            ['python', '../Interpreter/main.py'],
+            input=f"{data['code']}\n{chr(3)}\n{data['input']}".encode(),
+            capture_output=True
+        )
+        return HttpResponse(json.dumps({
+            "stdout": sb.stdout.decode(),
+            "stderr": sb.stderr.decode()
+        }))
     return HttpResponse("")
