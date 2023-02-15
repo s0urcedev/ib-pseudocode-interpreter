5b1ccdb640b8ecd34ec1cff18b4beaffa5a17b47
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-02-15 15:01:09 +0000

    Update html page

--- ibpseudocode/urls.py
+++ ibpseudocode/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from interpreter_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
     path('syntax', views.syntax)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
